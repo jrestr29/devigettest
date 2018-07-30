0998a9d70b9951e06c7cb824fa229a2fa03342ac
José Restrepo <Jose.X.Restrepo.-ND@Disney.com>
2018-07-30 15:45:27 -0500

    Widget initialization for required social networks widget

---
+++ app/code/Jose/Deviget/Block/Widget/Social/Block.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jose\Deviget\Block\Widget\Social;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Block extends Template implements BlockInterface
+{
+    protected $_template = "widget/social.phtml";
+
+}
